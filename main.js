9f6b44300bb1b386de92d04547d9374bb346a8b7
Ilja-Prihach <piratehnik9508@gmail.com>
2025-03-06 20:29:38 +0300

    counter shots and stats

--- main.js
+++ main.js
@@ -13,23 +13,30 @@ let guess;
 // Добавляем счетчик попаданий
 let hits = 0;
 
+// Добавляем счетчик выстрелов
+let = guesses = 0;
+
 while (isSunk === false) {
     // не забываем превратить строку в число
     guess = +prompt("Fire! Enter a number 0-6")
     if( guess < 0 || guess > 6) {
         alert("Please enter a valid cell number")
     } else {
-            // Логическое или
-    if (guess === location1 ||  guess === location2 ||  guess ===location3) {
-        alert ("Hit")
-        hits = hits + 1   // считаем попадания
-        if (hits === 3){
-            isSunk = true
-            alert("You sunk my battleship")
-        }   // если корабль потоплен
+        guesses = guesses + 1   // счетчик выстрелов
+            
+        if (guess === location1 ||  guess === location2 ||  guess ===location3) {
+            alert ("Hit")
+            hits = hits + 1   // считаем попадания
+            if (hits === 3){
+                isSunk = true
+                alert("You sunk my battleship")
+         }   // если корабль потоплен
 
-    } else {
-        alert ("Miss")
-    }
+        } else {
+            alert ("Miss")
+        }
     }
 } 
+
+const stats = "You have" + hits + "hits out of " + guesses + "shots."
+alert(stats)
